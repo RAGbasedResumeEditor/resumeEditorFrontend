d259db4d3bebf83ce9d94ea74c975ed02d395fe2
creatub <blatherr@gmail.com>
2024-10-26 23:19:15 +0900

    commit - List Design 수정

--- src/view/mainPage/resumeList/resumeList.tsx
+++ src/view/mainPage/resumeList/resumeList.tsx
@@ -1,12 +1,11 @@
-import axiosInstance from "@/api/api";
-import { Button, Form, Pagination } from "antd";
-import "./resumeList.css";
-import { EyeOutlined, StarOutlined } from "@ant-design/icons";
-import { useEffect, useState } from "react";
-import Search from "antd/es/input/Search";
-import { Link } from "react-router-dom";
-import React from "react";
-import axios from "axios";
+import axiosInstance from '@/api/api';
+import { Button, Form, Pagination } from 'antd';
+import './resumeList.css';
+import { EyeOutlined, StarOutlined } from '@ant-design/icons';
+import { useEffect, useState } from 'react';
+import Search from 'antd/es/input/Search';
+import { Link } from 'react-router-dom';
+import React from 'react';
 
 interface ResumeList {
   resumeBoardNo: number;
@@ -25,7 +24,7 @@ const ResumeList = () => {
   const [totalPages, setTotalPages] = useState<number>(0);
 
   async function fetchSearch(search: string) {
-    let res = await axiosInstance.get("/board/list/search", {
+    let res = await axiosInstance.get('/board/list/search', {
       params: {
         pageNo: 0,
         keyword: search,
@@ -36,7 +35,7 @@ const ResumeList = () => {
   }
 
   async function fetchList(page: number) {
-    let res = await axiosInstance.get("/board/list", {
+    let res = await axiosInstance.get('/board/list', {
       params: {
         pageNo: page - 1,
       },
@@ -46,12 +45,12 @@ const ResumeList = () => {
   }
 
   async function fetchTopRatedResumes(setter: (data: ResumeList[]) => void) {
-    let res = await axiosInstance.get("/board/list/rank/rating");
+    let res = await axiosInstance.get('/board/list/rank/rating');
     setter(res.data.response);
   }
 
   async function fetchTopReadResumes(setter: (data: ResumeList[]) => void) {
-    let res = await axiosInstance.get("/board/list/rank/read-count");
+    let res = await axiosInstance.get('/board/list/rank/read-count');
     setter(res.data.response);
   }
 
@@ -62,24 +61,24 @@ const ResumeList = () => {
   }, []);
 
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: 'flex' }}>
       <div
         className="leftWrapper"
         style={{
-          width: "30%",
-          padding: "1%",
-          marginLeft: "5%",
-          marginTop: "10.2%",
+          width: '30%',
+          padding: '1%',
+          marginLeft: '5%',
+          marginTop: '10.2%',
         }}
       >
         <div
           style={{
-            backgroundColor: "#0DC291",
-            padding: "1px",
-            borderRadius: "4px",
-            textAlign: "center",
-            marginBottom: "20px",
-            color: "white",
+            backgroundColor: '#0DC291',
+            padding: '1px',
+            borderRadius: '10px',
+            textAlign: 'center',
+            marginBottom: '20px',
+            color: 'white',
           }}
         >
           <h2>Top Rated Resumes</h2>
@@ -89,53 +88,66 @@ const ResumeList = () => {
             key={`topRated${index}`}
             className="contentBox"
             style={{
-              border: "1px solid rgb(225,225,225)",
-              padding: "2% 5%",
-              margin: "1% 0",
-              position: "relative",
-              display: "flex",
+              border: '1px solid #F9FAFB',
+              backgroundColor: '#F9FAFB',
+              padding: '2% 5%',
+              margin: '3% 0',
+              position: 'relative',
+              display: 'flex',
+              borderRadius: '10px',
             }}
           >
-            <img
-              src="/img/recommended.webp"
-              alt="Resume"
-              style={{
-                width: "60px",
-                height: "60px",
-                marginRight: "20px",
-                marginTop: "30px",
-              }}
-            />
-            <div style={{ flex: "1" }}>
-              <h4 style={{ color: "black", marginBottom: "4px" }}>
+            <div style={{ flex: '1' }}>
+              <h4 style={{ color: 'black', marginBottom: '4px' }}>
                 <Link
                   to={`/main/resumelist/${resume.resumeBoardNo}`}
-                  style={{ color: "black" }}
+                  style={{ color: '#0DC291' }}
                 >
                   {resume.title}
                 </Link>
               </h4>
               <p
                 style={{
-                  marginTop: "4px",
-                  fontSize: "13px",
-                  display: "-webkit-box",
-                  WebkitBoxOrient: "vertical",
+                  marginTop: '4px',
+                  fontSize: '13px',
+                  display: '-webkit-box',
+                  WebkitBoxOrient: 'vertical',
                   WebkitLineClamp: 2,
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
                 }}
               >
                 {resume.content}
               </p>
-              <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  fontSize: '12px',
+                }}
+              >
                 <span>{resume.createdDate.slice(0, 10)}</span>
                 <span>
                   <EyeOutlined /> {resume.readCount}
                 </span>
               </div>
-              <div style={{ position: "absolute", top: "10px", right: "10px" }}>
-                <StarOutlined /> {resume.rating.toFixed(1)}
+              <div
+                style={{
+                  position: 'absolute',
+                  top: '-38px',
+                  right: '-15px',
+                }}
+              >
+                <img
+                  src="/img/RT_icon_like_2.svg"
+                  alt="Top Rated Icon"
+                  style={{
+                    width: '100px',
+                    height: '100px',
+                    filter:
+                      'invert(57%) sepia(58%) saturate(351%) hue-rotate(113deg) brightness(100%) contrast(91%)',
+                  }}
+                />
               </div>
             </div>
           </div>
@@ -143,13 +155,13 @@ const ResumeList = () => {
 
         <div
           style={{
-            backgroundColor: "#0DC291",
-            padding: "1px",
-            borderRadius: "4px",
-            textAlign: "center",
-            marginTop: "50px",
-            marginBottom: "20px",
-            color: "white",
+            backgroundColor: '#0DC291',
+            padding: '1px',
+            borderRadius: '10px',
+            textAlign: 'center',
+            marginTop: '50px',
+            marginBottom: '20px',
+            color: 'white',
           }}
         >
           <h2>Most Viewed Resumes</h2>
@@ -159,66 +171,79 @@ const ResumeList = () => {
             key={`topRead${index}`}
             className="contentBox"
             style={{
-              border: "1px solid rgb(225,225,225)",
-              padding: "2% 5%",
-              margin: "1% 0",
-              position: "relative",
-              display: "flex",
+              border: '1px solid #F9FAFB',
+              backgroundColor: '#F9FAFB',
+              padding: '2% 5%',
+              margin: '3% 0',
+              position: 'relative',
+              display: 'flex',
+              borderRadius: '10px',
             }}
           >
-            <img
-              src="/img/most_viewed.webp"
-              alt="Resume"
-              style={{
-                width: "60px",
-                height: "60px",
-                marginRight: "20px",
-                marginTop: "30px",
-              }}
-            />
-            <div style={{ flex: "1" }}>
-              <h4 style={{ color: "black", marginBottom: "4px" }}>
+            <div style={{ flex: '1' }}>
+              <h4 style={{ color: 'black', marginBottom: '4px' }}>
                 <Link
                   to={`/main/resumelist/${resume.resumeBoardNo}`}
-                  style={{ color: "black" }}
+                  style={{ color: '#0DC291' }}
                 >
                   {resume.title}
                 </Link>
               </h4>
               <p
                 style={{
-                  fontSize: "13px",
-                  marginTop: "4px",
-                  display: "-webkit-box",
-                  WebkitBoxOrient: "vertical",
+                  fontSize: '13px',
+                  marginTop: '4px',
+                  display: '-webkit-box',
+                  WebkitBoxOrient: 'vertical',
                   WebkitLineClamp: 2,
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
                 }}
               >
                 {resume.content}
               </p>
-              <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  fontSize: '12px',
+                }}
+              >
                 <span>{resume.createdDate.slice(0, 10)}</span>
                 <span>
                   <EyeOutlined /> {resume.readCount}
                 </span>
               </div>
-              <div style={{ position: "absolute", top: "10px", right: "10px" }}>
-                <StarOutlined /> {resume.rating.toFixed(1)}
+              <div
+                style={{
+                  position: 'absolute',
+                  top: '-38px',
+                  right: '-15px',
+                }}
+              >
+                <img
+                  src="/img/RT_icon_view_2.svg"
+                  alt="Most Viewed Icon"
+                  style={{
+                    width: '100px',
+                    height: '100px',
+                    filter:
+                      'invert(57%) sepia(58%) saturate(351%) hue-rotate(113deg) brightness(100%) contrast(91%)',
+                  }}
+                />
               </div>
             </div>
           </div>
         ))}
       </div>
 
-      <div className="rightWrapper" style={{ width: "75%", padding: "1%" }}>
+      <div className="rightWrapper" style={{ width: '75%', padding: '1%' }}>
         <div
           className="search"
           style={{
-            display: "flex",
-            justifyContent: "center",
-            paddingTop: "3%",
+            display: 'flex',
+            justifyContent: 'center',
+            paddingTop: '3%',
           }}
         >
           <Search
@@ -227,11 +252,11 @@ const ResumeList = () => {
             }}
             size="large"
             placeholder="검색어를 입력하세요"
-            style={{ width: "60%" }}
+            style={{ width: '60%' }}
           />
         </div>
         <div
-          style={{ display: "flex", justifyContent: "center", margin: "3% 0" }}
+          style={{ display: 'flex', justifyContent: 'center', margin: '3% 0' }}
         >
           <Pagination
             onChange={(page) => {
@@ -245,26 +270,28 @@ const ResumeList = () => {
         <div className="wrapper">
           <div
             className="innerWrapper"
-            style={{ display: "flex", width: "100%", flexDirection: "column" }}
+            style={{ display: 'flex', width: '100%', flexDirection: 'column' }}
           >
             {resumeList.map((resume, index) => (
               <div
                 key={`resume.r_num${index}`}
-                style={{ display: "flex", justifyContent: "center" }}
+                style={{ display: 'flex', justifyContent: 'center' }}
               >
                 <div
                   className="contentBox"
                   style={{
-                    width: "80%",
-                    border: "1px solid rgb(225,225,225)",
-                    padding: "2% 5%",
-                    margin: "1% 0",
+                    width: '80%',
+                    border: '1px solid #F9FAFB',
+                    backgroundColor: '#F9FAFB',
+                    borderRadius: '10px',
+                    padding: '2% 3%',
+                    margin: '1% 0',
                   }}
                 >
                   <div className="contentTitle">
                     <h3>
                       <Link
-                        style={{ color: "black" }}
+                        style={{ color: '#0DC291' }}
                         to={`/main/resumelist/${resume.resumeBoardNo}`}
                       >
                         {resume.title}
@@ -274,14 +301,14 @@ const ResumeList = () => {
                   <div
                     className="contentMainText"
                     style={{
-                      display: "-webkit-box",
-                      WebkitBoxOrient: "vertical",
-                      margin: "1% 0",
-                      fontSize: "16px",
-                      overflow: "hidden",
-                      textOverflow: "ellipsis",
+                      display: '-webkit-box',
+                      WebkitBoxOrient: 'vertical',
+                      margin: '1% 0',
+                      fontSize: '16px',
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
                       WebkitLineClamp: 2,
-                      width: "100%",
+                      width: '100%',
                     }}
                   >
                     {resume.content}
@@ -289,16 +316,17 @@ const ResumeList = () => {
                   <div
                     className="contentFooter"
                     style={{
-                      justifyContent: "space-between",
-                      display: "flex",
-                      fontSize: "14px",
+                      justifyContent: 'space-between',
+                      display: 'flex',
+                      fontSize: '14px',
                     }}
                   >
                     <div className="contentDate">
                       {resume.createdDate.slice(0, 10)}
                     </div>
-                    <div className="contentView">
-                      <EyeOutlined /> {resume.readCount}
+                    <div className="view-count">
+                      <EyeOutlined /> {resume.readCount} <StarOutlined />{' '}
+                      {resume.rating.toFixed(1)} ({resume.ratingCount})
                     </div>
                   </div>
                 </div>
